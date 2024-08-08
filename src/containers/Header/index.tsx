7b44e144181586bb308dcd8cac424472ae5f317f
flotoine <florian.antoine.m@gmail.com>
2024-08-08 16:55:05 +0200

    first commit

---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+      <div>
+        <div>
+            Florian Antoine
+        </div>
+        <div>
+            Développeur Front et Back
+        </div>
+      </div>
+    );
+  }
---
+++ src/containers/Projects/index.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+      <div>
+        Projets 
+      </div>
+    );
+  }
+  // va contenir cartes projet (component)
