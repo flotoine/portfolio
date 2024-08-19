220816f82db248bb1aefee4ec7cc37f2f6fb0531
flotoine <florian.antoine.m@gmail.com>
2024-08-19 13:01:54 +0200

    Dark mode completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:true,
-    i18n:{
-        locales:["fr",'en'],
-        defaultLocale:"fr",
-        localeDetection:true
-    }
-
 };
 
 export default nextConfig;
