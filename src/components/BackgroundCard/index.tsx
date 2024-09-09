59ec86c3f891e6234fca9edc1a92abe3daae45f7
flotoine <florian.antoine.m@gmail.com>
2024-09-09 09:51:07 +0200

    Background table draft advanced

--- src/components/BackgroundCard/index.tsx
+++ src/components/BackgroundCard/index.tsx
@@ -1,18 +1,20 @@
 import { useTranslations } from "next-intl"
-import backgroundList from './background.json'
+
 
 export default function BackgroundCard () {
 
 const t = useTranslations('Skills')
-
+const backgroundTable = [0,1,2,3,4,5,6]
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-wrap gap-4 mx-auto my-12 w-10/12">
             {
-                backgroundList.map((backgroundItem)=>
-                    <div key={backgroundItem.id}>
-                        <div>{t(`BackgroundList.${backgroundItem.id}.title`)}</div>
-                        <div>{t(`BackgroundList.${backgroundItem.id}.date`)}</div>
-                        <div>{t(`BackgroundList.${backgroundItem.id}.place`)}</div>
+                backgroundTable.map((backgroundItem)=>
+                    <div key={backgroundItem} className="bg-zinc-300 h-36 w-52 flex flex-col justify-around px-4">
+                        <div>
+                            <div className="font-bold">{t(`BackgroundList.${backgroundItem}.title`)}</div>
+                            <div>{t(`BackgroundList.${backgroundItem}.place`)}</div>
+                        </div>
+                        <div>+</div>
                     </div>)
             }
         </div>
