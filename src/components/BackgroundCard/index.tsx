c0684c737fd8b16dc3f84d003ae89cd2e81bbdfa
flotoine <florian.antoine.m@gmail.com>
2024-09-11 17:38:29 +0200

    added info on background card click

--- src/components/BackgroundCard/index.tsx
+++ src/components/BackgroundCard/index.tsx
@@ -1,7 +1,9 @@
 "use client"
 import { useTranslations } from "next-intl"
+import { motion, LayoutGroup } from 'framer-motion'
 
 import './style.css'
+import { useEffect } from "react"
 
 export default function BackgroundCard () {
 
@@ -21,10 +23,15 @@ const handleClick = (backgroundItem:number) => {
     try{document.getElementsByClassName('expanded')[0].classList.remove('expanded')}catch(err){}
     const clickedCard = document.getElementById(`card${backgroundItem}`) as HTMLInputElement
     const changingButton = document.getElementById(`changingButton--card${backgroundItem}`) as HTMLInputElement
+    const backgroundExtra = document.getElementById(`backgroundExtra--card${backgroundItem}`) as HTMLInputElement
     if (previousCard == clickedCard) {
         clickedCard.classList.remove('expanded')
+        backgroundExtra.classList.remove('visible')
+        backgroundExtra.classList.add('invisible')
     } else {
         clickedCard.classList.add('expanded')
+        backgroundExtra.classList.add('visible')
+        backgroundExtra.classList.remove('invisible')
     }
     try{clickedCard.classList.contains('expanded')? changingButton.textContent="-":changingButton.textContent="+"}catch(err){console.log(err)}
 }
@@ -34,12 +41,16 @@ const handleClick = (backgroundItem:number) => {
         <div className="grid grid-cols-3 grid-flow-dense gap-4 mx-auto my-12 w-3/4 ">
             {
                 backgroundTable.map((backgroundItem)=>
-                    <div key={backgroundItem} id={`card${backgroundItem}`}
+                    <div  key={backgroundItem} id={`card${backgroundItem}`}
                        className="bg-zinc-300 dark:bg-zinc-800 dark:text-zinc-50 flex flex-col justify-around px-4 aspect-square">
                         <div>
                             <div className="font-bold">{t(`BackgroundList.${backgroundItem}.title`)}</div>
                             <div>{t(`BackgroundList.${backgroundItem}.place`)}</div>
                         </div>
+                        <div id={`backgroundExtra--card${backgroundItem}`} className="invisible">
+                            <div>{t(`BackgroundList.${backgroundItem}.location`)}</div>   
+                            <div>{t(`BackgroundList.${backgroundItem}.date`)}</div>
+                        </div>    
                         <div>
                             <button id={`changingButton--card${backgroundItem}`} onClick={()=>handleClick(backgroundItem)}>+</button>
                         </div>
