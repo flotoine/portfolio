ec7b05715a4e2c21a1a325e0bee1b11bc29e9479
flotoine <florian.antoine.m@gmail.com>
2024-09-11 21:13:14 +0200

    pre intl fix

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -27,29 +27,32 @@ export default function Header() {
 
 
     return (
-      <div className='header-container static flex flex-col place-items-center  bg-zinc-50 dark:bg-zinc-900'> 
-        <div className=" flex flex-col font-outline-1  h-[calc(100dvh-65px)] place-content-center   transition-all text-zinc-950  dark:text-zinc-50 dark:font-outline-0">
-          <h1 className="text-8xl z-10">
-              Florian Antoine
-          </h1>
-          <p className="text-[57px] font-light z-10 ">
-            {t('title')}
-          </p>
+      <div>
+        <div className='header-container static flex flex-col place-items-center  bg-zinc-50 dark:bg-zinc-900 '> 
+          <div className=" flex flex-col font-outline-1  h-[calc(100dvh-65px)] place-content-center   transition-all text-zinc-950  dark:text-zinc-50 dark:font-outline-0">
+            <h1 className="text-8xl z-10">
+                Florian Antoine
+            </h1>
+            <p className="text-[57px] font-light z-10 ">
+              {t('title')}
+            </p>
+          </div>
+          <div className='z-0 drop-shadow-lg brightness-200 opacity-50 absolute top-10 '>
+            <Image 
+              src={profilePic}
+              alt="Photo d'identité de Florian Antoine"
+              width={600}
+              height={800}
+              className='PP'
+            />
+          </div>
+          
+          
         </div>
-        <div className='z-0 drop-shadow-lg brightness-200 opacity-50 absolute top-10 '>
-          <Image 
-            src={profilePic}
-            alt="Photo d'identité de Florian Antoine"
-            width={600}
-            height={800}
-            className='PP'
-          />
+        <div className='absolute top-5 right-5 flex flex-row place-items-center z-100 dark:invert'>
+            <Language />
+            <FontAwesomeIcon icon={faMoon} onClick={toggleTheme} />
         </div>
-        <div className='absolute top-5 right-5 flex flex-row place-items-center dark:invert'>
-          <Language />
-          <FontAwesomeIcon icon={faMoon} onClick={toggleTheme} />
-        </div>
-        
       </div>
     );
   }
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -6,7 +6,7 @@ export const routing = defineRouting({
   locales: ['fr', 'en'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'fr'
 });
  
 // Lightweight wrappers around Next.js' navigation APIs
