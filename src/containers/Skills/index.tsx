8cf2aee581b0884bc7a26af69951df4cf2160334
flotoine <florian.antoine.m@gmail.com>
2024-09-23 18:11:48 +0200

    shadow cards skills

--- src/containers/Skills/index.tsx
+++ src/containers/Skills/index.tsx
@@ -32,11 +32,11 @@ export default function Skills() {
       <div className="bg-stone-300 px-8 py-4 dark:bg-stone-700 ">
         <h2 className="text-stone-900 text-3xl pb-4 dark:text-stone-100">{t('title')}</h2>
         <div className="flex gap-4 justify-between flex-col md:flex-row">  
-          <section className="flex flex-col backdrop-brightness-90 text-center rounded">
+          <section className="flex flex-col backdrop-brightness-90 text-center rounded shadow-lg">
             <h3 className="text-stone-900 dark:text-stone-100 text-xl font-light py-4">{t('techs')}</h3>
             <EmblaCarousel/>
           </section>
-          <section className="backdrop-brightness-90 flex-grow rounded">
+          <section className="backdrop-brightness-90 flex-grow rounded shadow-lg">
             <h3 className="text-stone-900 dark:text-stone-100 text-xl text-center font-light py-4">{t('otherskills')}</h3>
             <div className="grid grid-cols-[18px_auto] gap-x-2 gap-y-1 p-2 items-center text-stone-900 dark:text-stone-100">
               <Image src="/assets/skillLogos/html.png" alt="logo HTML" height={30} width={30} />
